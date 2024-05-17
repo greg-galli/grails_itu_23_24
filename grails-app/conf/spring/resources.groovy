2d89861ba9c0b4ca894e54c61033be3dcd678c9e
Gregory Galli <greg.galli@gmail.com>
2024-05-17 13:27:46 +0200

    Model ok before association testing

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
+import com.itu.mbds.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener)
 }
---
+++ grails-app/domain/com/itu/mbds/Comment.groovy
@@ -0,0 +1,19 @@
+package com.itu.mbds
+
+class Comment {
+
+    String content
+    Date dateCreated
+    Date lastUpdated
+    Integer likeCount = 0
+    User author
+
+    static belongsTo = [post: Post]
+
+    static constraints = {
+        content blank: false, nullable: false
+        likeCount nullable: false, min: 0
+        author nullable: false
+        post nullable: false
+    }
+}
