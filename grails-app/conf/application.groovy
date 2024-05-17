10ba36739eb7cec36076af856021947dacc276eb
Gregory Galli <greg.galli@gmail.com>
2024-05-17 15:04:57 +0200

    Generated Controllers & Views

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -4,8 +4,9 @@
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.itu.mbds.User'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.itu.mbds.UserRole'
 grails.plugin.springsecurity.authority.className = 'com.itu.mbds.Role'
+grails.plugin.springsecurity.logout.postOnly = false
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
-	[pattern: '/**',               access: ['permitAll']],
+	[pattern: '/',               access: ['permitAll']],
 	[pattern: '/error',          access: ['permitAll']],
 	[pattern: '/index',          access: ['permitAll']],
 	[pattern: '/index.gsp',      access: ['permitAll']],
--- grails-app/views/layouts/main.gsp
+++ grails-app/views/layouts/main.gsp
@@ -29,6 +29,12 @@
             </ul>
         </div>
     </div>
+    <sec:ifLoggedIn>
+        <g:link controller="logout">Logout</g:link>
+    </sec:ifLoggedIn>
+    <sec:ifNotLoggedIn>
+        <g:link controller="login">Login</g:link>
+    </sec:ifNotLoggedIn>
 </nav>
 
 <g:layoutBody/>
