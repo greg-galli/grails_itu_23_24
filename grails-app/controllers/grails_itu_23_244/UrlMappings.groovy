fd122211c59c266906f84f0b0e724069ea53120e
Gregory Galli <greg.galli@gmail.com>
2024-05-17 10:53:34 +0200

    Initial empty project

---
+++ grails-app/controllers/grails_itu_23_244/UrlMappings.groovy
@@ -0,0 +1,16 @@
+package grails_itu_23_244
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+    }
+}
---
+++ grails-app/init/grails_itu_23_244/BootStrap.groovy
@@ -0,0 +1,9 @@
+package grails_itu_23_244
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
